c136d26dc0d5501eb497ce64666a9d9bd2a85e9c
Alicia <alicia.ibanez.escudero@gmail.com>
2025-02-13 21:49:32 +0100

    h

--- Perro.java
+++ Perro.java
@@ -10,7 +10,7 @@ public class Perro
     private String tipoPelo;
     private int edad;
     private boolean ppp;
-
+    private boolean tieneChip;
     /**
      * Constructor for objects of class Perro
      */
@@ -19,6 +19,7 @@ public class Perro
         ppp = false;
         edad = esEdad;
         tipoPelo = esTipoPelo;
+        tieneChip = true;
     }
 
     /**
@@ -76,4 +77,11 @@ public class Perro
         return "Tipo de pelo: " + tipoPelo + "Edad: " + edad + (ppp ? "Verdadero" : "Falso");
     }
     
+    public boolean getTieneChip () {
+        return tieneChip;
+    }
+    
+    public void SetTieneChip (boolean esChip) {
+        tieneChip=esChip;
+    }
 }
